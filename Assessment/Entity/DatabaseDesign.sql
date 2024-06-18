ef00819d458103540867913181b4c11c3862c745
Jessa Delo-Ag <jessa.delo-ag@bcstechnology.com.au>
2024-06-18 19:46:19 +0800

    zapper assessment

---
+++ Assessment/Entity/DatabaseDesign.sql
@@ -0,0 +1,41 @@
+﻿-- store user information 
+CREATE TABLE Users (
+    user_id INT NOT NULL IDENTITY(1, 1),
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- store product information
+CREATE TABLE Products (
+    product_id INT NOT NULL IDENTITY(1, 1),
+    product_name VARCHAR(100) NOT NULL,
+    product_description TEXT,
+    price DECIMAL(10, 2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- store general transaction information
+CREATE TABLE Transactions (
+    transaction_id INT NOT NULL IDENTITY(1, 1),
+    user_id INT,
+    transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    total_amount DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+-- store details of each transaction
+CREATE TABLE TransactionDetails (
+    transaction_detail_id INT NOT NULL IDENTITY(1, 1),
+    transaction_id INT,
+    product_id INT,
+    quantity INT NOT NULL,
+    price_at_purchase DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (transaction_id) REFERENCES Transactions(transaction_id),
+    FOREIGN KEY (product_id) REFERENCES Products(product_id)
+);
+
+-- Indexes to speed up queries
+CREATE INDEX idx_user_id ON Transactions(user_id);
+CREATE INDEX idx_transaction_id ON TransactionDetails(transaction_id);
+CREATE INDEX idx_product_id ON TransactionDetails(product_id);
---
+++ Assessment/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+namespace BooleanOutputApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(IsFeatureEnabled("00000000", 7));
+            Console.WriteLine(IsFeatureEnabled("00000010", 7));
+            Console.WriteLine(IsFeatureEnabled("11111111", 4));
+            Console.Read();
+
+            ////write settings to file////
+            WriteSettingsToFile("11111111");
+            Console.Read();
+
+            ////read settings from file////
+            Console.WriteLine(ReadSettings("./settings.txt"));
+            Console.Read();
+
+        }
+
+        public static bool IsFeatureEnabled(string settings, int setting)
+        {
+            //get actual index of setting
+            int index = setting - 1;
+
+            //convert string to array
+            string[] settingsArray = settings.Select(x => x.ToString()).ToArray();
+
+            //check if index value is equal to 1
+            return settingsArray[index] == "1";
+        }
+
+        public static void WriteSettingsToFile(string settings)
+        {
+
+            using (StreamWriter writetext = new StreamWriter("settings.txt"))
+            {
+                writetext.WriteLine(settings);
+            }
+            FileStream ostrm;
+            StreamWriter writer;
+            TextWriter oldOut = Console.Out;
+            try
+            {
+                ostrm = new FileStream("./settings.txt", FileMode.OpenOrCreate, FileAccess.Write);
+                writer = new StreamWriter(ostrm);
+                writer.WriteLine(settings);
+                writer.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Cannot open settings.txt for writing");
+                Console.WriteLine(e.Message);
+                return;
+            }
+        }
+
+        public static string ReadSettings(string filepath)
+        {
+            string settings = File.ReadAllText(filepath);
+            return settings;
+        }
+    }
+}
